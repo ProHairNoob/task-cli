d45d93245b58aa65cc632fad81c2c355206d45ae
Yakoub <yakoubbeneyad@gmail.com>
2026-06-30 10:59:56 +0200

    implemented saving and loading tasks in storage.py

---
+++ storage.py
@@ -0,0 +1,15 @@
+import json
+from pathlib import Path
+
+file_path = Path("tasks.json")
+
+def load_task():
+    if not file_path.exists():
+        return []
+    with file_path.open("r") as f:
+        return json.load(f)
+
+def save_task(tasks):
+    with file_path.open("w") as f:
+         json.dump(tasks,f)
+
