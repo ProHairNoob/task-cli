42949d1256ce78ccdd081fe42010a8ec69d2fe12
Yakoub <yakoubbeneyad@gmail.com>
2026-07-01 09:06:39 +0200

    Added add command and list command

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import argparse
-
+from tasks import add_cmd , list_cmd
 parser = argparse.ArgumentParser(
     prog="task-cli",
     description="manage your tasks"
 )
+subparsers = parser.add_subparsers(dest="command")
+
+
+add = subparsers.add_parser("add")
+add.add_argument("description")
+list = subparsers.add_parser("list")
+args = parser.parse_args()
+
+if args.command == "add":
+    add_cmd(args.description) 
+    print(f"task added {args.description}")
 
 
 
+if args.command == "list":
+    list_cmd()
 
